fe46ae1e8c2d7459633d3c2de454ee75a6fe73ca
pablo <pablo.santos@gstecnologia-al.com.br>
2023-12-26 16:03:59 -0300

    add | produtos ao carrinho

---
+++ src/redux/cart/reducer.js
@@ -0,0 +1,20 @@
+import CartActionTypes from "./action-types"
+
+const initialState ={
+    products:[],
+    productsTotalPrice:0
+}
+
+const cartReducer = (state = initialState, action) =>{
+        switch(action.type){
+            case CartActionTypes.ADD_PRODUCT:
+                return {
+                    ...initialState,
+                     products:[...initialState.products, action.payload],
+                }
+            default:
+                return state;
+        }
+}
+
+export default cartReducer
