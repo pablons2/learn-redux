fcb113ae810c4a38b3542c7d19a0a44c32b4d241
pablo <pablo.santos@gstecnologia-al.com.br>
2023-12-27 10:26:08 -0300

    add | count in the cart

--- src/components/cart/index.jsx
+++ src/components/cart/index.jsx
@@ -1,17 +1,19 @@
-import {useSelector} from 'react-redux'
-// Styles
+import { useSelector } from 'react-redux';
 import * as Styles from "./styles";
-import rootReducer from '../../redux/root-reducer';
 import CartItem from '../cart-item';
+
 const Cart = ({ isVisible, setIsVisible }) => {
   const handleEscapeAreaClick = () => setIsVisible(false);
-  const {products} = useSelector(rootReducer =>rootReducer.cartReducer)
+  
+  // Utilize o useSelector para extrair diretamente o estado do cartReducer
+  const { products } = useSelector(state => state.cartReducer);
+
   return (
     <Styles.CartContainer isVisible={isVisible}>
       <Styles.CartEscapeArea onClick={handleEscapeAreaClick} />
       <Styles.CartContent>
         <Styles.CartTitle>Seu Carrinho</Styles.CartTitle>
-        {products.map(product =><CartItem product={product}/>)}
+        {products.map(product => <CartItem product={product} key={product.id} />)}
       </Styles.CartContent>
     </Styles.CartContainer>
   );
--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,5 +1,5 @@
 "use client"
-import { useState } from "react";
+import { useState, useMemo } from "react";
 import {useSelector, useDispatch } from 'react-redux'
 import UserActionTypes from "../../redux/user/action-types";
 // Components
@@ -11,39 +11,46 @@ import * as Styles from "./styles";
 function Header() {
   const [cartIsVisible, setCartIsVisible] = useState(false);
 
-  const {currentUser}= useSelector(rootReducer=>rootReducer.userReducer);
-  const dispatch = useDispatch()
-  console.log({currentUser})
+  const { currentUser } = useSelector((rootReducer) => rootReducer.userReducer);
+  const { products } = useSelector((rootReducer) => rootReducer.cartReducer);
+
+  const dispatch = useDispatch();
+
+  const productsCount = useMemo(() => {
+    return products.reduce((acc, curr) => acc + curr.quantity, 0); // Fix typo here
+  }, [products]);
+
   const handleCartClick = () => {
     setCartIsVisible(true);
   };
 
-const handleLoginClick = ()=>{
-  dispatch({
-    type:UserActionTypes.LOGIN,
-    payload: { name: "pabloN", email:"pablo@pablo.com"},
-    // qualquercoisa: "qualquer coisa mesmo"
-    
-  })
+  const handleLoginClick = () => {
+    dispatch({
+      type: UserActionTypes.LOGIN,
+      payload: { name: "pabloN", email: "pablo@pablo.com" },
+    });
+  };
 
-}
-const handleLogoutClick = ()=>{
-  dispatch({
-    type: UserActionTypes.LOGOUT
-   
-  })
-}
+  const handleLogoutClick = () => {
+    dispatch({
+      type: UserActionTypes.LOGOUT,
+    });
+  };
+
   return (
     <Styles.Container>
       <Styles.Logo>Redux Shopping</Styles.Logo>
       <Styles.Buttons>
-        {currentUser? <div ><span>Bem vindo, <b>{currentUser.name}</b>! </span> <span onClick={handleLogoutClick}>sair</span> </div>  :
-        (<div onClick={handleLoginClick}>Login</div>)}
-        
-        <div onClick={handleCartClick}>Carrinho</div>
-     
-       
-        
+        {currentUser ? (
+          <div>
+            <span>Bem vindo, <b>{currentUser.name}</b>! </span>
+            <span onClick={handleLogoutClick}>sair</span>
+          </div>
+        ) : (
+          <div onClick={handleLoginClick}>Login</div>
+        )}
+
+        <div onClick={handleCartClick}>Carrinho ({productsCount}) </div>
       </Styles.Buttons>
 
       <Cart isVisible={cartIsVisible} setIsVisible={setCartIsVisible} />
